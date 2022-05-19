17c31ec1719b37f7601d33dcaaaec0b706fceee9
Hirano <hnaoya1120@gmail.com>
2022-05-20 00:46:15 +0900

    データベースと接続

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ app.use(accesslogger());
 
 // Dynamic resource rooting.
 app.use("/", require("./routes/index.js"));
+app.use("/test", async (req, res, next) => {
+  const { MySQLClient, sql } = require("./lib/database/client.js");
+  var data;
+
+  try {
+    data = await MySQLClient.executeQuery(await sql("SELECT_SHOP_BASIC_BY_ID"), [1]);
+    console.log(data);
+  } catch (err) {
+    next(err);
+  }
+
+  res.end("OK");
+});
 
 // Set application log.
 app.use(applicationlogger());
